5cf393ee8205f3c66c49411290105a3a51c28999
Jonatan Lindh <jonatan.lindh1@gmail.com>
2026-01-13 18:58:41 +0100

    temporary hyprland uwsm fix

--- modules/nixos/desktop/hyprland.nix
+++ modules/nixos/desktop/hyprland.nix
@@ -4,16 +4,46 @@
   ...
 }:
 {
-  # programs.uwsm.waylandCompositors.hyprland.binPath = pkgs.lib.mkForce ''
-  #   ${inputs.hyprland.packages.${pkgs.system}.hyprland}/bin/start-hyprland
-  # '';
-
   programs.hyprland = {
     enable = true;
     withUWSM = true;
     package = perSystem.hyprland.hyprland;
     portalPackage = perSystem.hyprland.xdg-desktop-portal-hyprland;
   };
+
+  services.displayManager.sessionPackages = [
+    (pkgs.runCommand "hyprland-gdm-fix"
+      {
+        passthru.providedSessions = [
+          "hyprland-fixed"
+          "hyprland-uwsm-fixed"
+        ];
+      }
+      ''
+        mkdir -p $out/share/wayland-sessions
+
+        # 1. Copy and Patch UWSM version
+        cp ${perSystem.hyprland.hyprland}/share/wayland-sessions/hyprland-uwsm.desktop \
+           $out/share/wayland-sessions/hyprland-uwsm-fixed.desktop
+
+        # Fix Validation
+        sed -i 's/^DesktopNames=/X-DesktopNames=/' $out/share/wayland-sessions/hyprland-uwsm-fixed.desktop
+        # Change Name
+        sed -i 's/^Name=.*/Name=Hyprland (UWSM-Fixed)/' $out/share/wayland-sessions/hyprland-uwsm-fixed.desktop
+
+        # FORCE ABSOLUTE PATHS FOR GDM
+        # Replace 'Exec=uwsm' with 'Exec=/nix/store/.../bin/uwsm'
+        sed -i "s#Exec=uwsm#Exec=${pkgs.uwsm}/bin/uwsm#" $out/share/wayland-sessions/hyprland-uwsm-fixed.desktop
+        sed -i "s#TryExec=uwsm#TryExec=${pkgs.uwsm}/bin/uwsm#" $out/share/wayland-sessions/hyprland-uwsm-fixed.desktop
+
+        # 2. Copy and Patch Vanilla version
+        cp ${perSystem.hyprland.hyprland}/share/wayland-sessions/hyprland.desktop \
+           $out/share/wayland-sessions/hyprland-fixed.desktop
+        sed -i 's/^DesktopNames=/X-DesktopNames=/' $out/share/wayland-sessions/hyprland-fixed.desktop
+        sed -i 's/^Name=.*/Name=Hyprland (Fixed)/' $out/share/wayland-sessions/hyprland-fixed.desktop
+      ''
+    )
+  ];
 
   xdg.portal = {
     enable = true;
